dbc6f5522edfbf73c8e9317fb0850d0391410add
Bogdan <bogdan_uman@meta.ua>
2019-02-28 18:34:55 +0200

    change seeds

--- seeders/20190110100918-demo-devices.js
+++ seeders/20190110100918-demo-devices.js
@@ -1,24 +1,14 @@
+const tableName = 'Devices';
 
+const devices = Array.from(
+  { length: 1000 },
+  (v, i) => ({
+    code: `1${(i + 1).toString().padStart(4, '0')}`,
+    name: `Устройство: ${i + 1}`
+  })
+);
 
 module.exports = {
-  up: queryInterface => queryInterface.bulkInsert('Devices', [
-    {
-      code: '1',
-      name: 'Coffee Shop Lvov'
-    },
-    {
-      code: '2',
-      name: 'Coffee SHop Kiev'
-    },
-    {
-      code: '3',
-      name: 'Coffee Shop Rivne'
-    },
-    {
-      code: '4',
-      name: 'Coffee SHop Lutsk'
-    }
-  ], {}),
-
-  down: queryInterface => queryInterface.bulkDelete('Devices', null, {})
+  up: queryInterface => queryInterface.bulkInsert(tableName, devices, {}),
+  down: queryInterface => queryInterface.bulkDelete(tableName, null, {})
 };
