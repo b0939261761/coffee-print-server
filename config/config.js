143fd8b4b7ac5b6c9356ed020cb754300b22d2ec
b0939261761 <b0939261761@gmail.com>
2019-04-27 13:30:16 +0300

    update package

--- config/config.js
+++ config/config.js
@@ -1,14 +1,11 @@
-
 const defaultValue = {
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   host: process.env.POSTGRES_HOST,
   port: process.env.POSTGRES_PORT,
-  dialect: 'postgres',
-  operatorsAliases: false
+  dialect: 'postgres'
 };
-
 
 module.exports = {
   development: {
--- unzip.js
+++
@@ -1,6 +0,0 @@
-const util = require('util');
-const ApkReader = require('adbkit-apkreader');
-
-ApkReader.open('./uploads/app/whimsy.apk')
-  .then(reader => reader.readManifest())
-  .then(manifest => console.log(manifest.versionName));
