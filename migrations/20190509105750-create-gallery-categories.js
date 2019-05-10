1a3b5874bd4ac6dead43c467129087be13473160
b0939261761 <b0939261761@gmail.com>
2019-05-10 19:27:13 +0300

    move to new server

---
+++ migrations/20190509105750-create-gallery-categories.js
@@ -0,0 +1,37 @@
+// const tableName = 'GalleryCategories';
+
+// module.exports = {
+//   up: (queryInterface, Sequelize) => queryInterface.createTable(tableName, {
+//     id: {
+//       allowNull: false,
+//       autoIncrement: true,
+//       primaryKey: true,
+//       type: Sequelize.INTEGER
+//     },
+//     num: {
+//       allowNull: false,
+//       type: Sequelize.INTEGER,
+//       defaultValue: 0
+//     },
+//     name: {
+//       allowNull: false,
+//       type: Sequelize.STRING(50),
+//       defaultValue: ''
+//     },
+//     createdAt: {
+//       allowNull: false,
+//       type: Sequelize.DATE,
+//       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+//     },
+//     updatedAt: {
+//       allowNull: false,
+//       type: Sequelize.DATE,
+//       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+//     }
+//   }).then(() => queryInterface.sequelize.query(`
+//     CREATE TRIGGER ${tableName}_update_at
+//     BEFORE UPDATE ON "${tableName}"
+//       FOR EACH ROW EXECUTE PROCEDURE update_at_timestamp()
+//   `)),
+//   down: queryInterface => queryInterface.dropTable(tableName)
+// };
