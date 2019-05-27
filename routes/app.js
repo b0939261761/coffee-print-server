0ee164a54d2c8f1adcfce8be15ab6ef3a896f94d
b0939261761 <b0939261761@gmail.com>
2019-05-27 15:36:40 +0300

    fix upgrade app

--- routes/app.js
+++ routes/app.js
@@ -3,14 +3,11 @@
 const fs = require('fs');
 const path = require('path');
 const multer = require('multer');
-const util = require('util');
 const routes = require('express').Router();
+const fsPromises = require('fs').promises;
 const ApkReader = require('adbkit-apkreader');
 const models = require('../models');
 const compareVersion = require('../utils/compareVersion');
-
-const { Op } = models.Sequelize;
-const rename = util.promisify(fs.rename);
 
 const pathApp = 'app';
 const pathUploads = path.join(__dirname, '..', process.env.APP_PATH_UPLOADS, pathApp);
@@ -19,12 +16,14 @@ const filenameApp = 'whimsy.apk';
 const fullPathApp = path.join(pathUploads, filenameApp);
 const fullPathAppTmp = path.join(pathTmp, filenameApp);
 
+if (!fs.existsSync(pathTmp)) fs.mkdirSync(pathTmp);
+if (!fs.existsSync(pathUploads)) fs.mkdirSync(pathUploads);
+
 const fileFilter = (req, file, next) => {
   if (file.mimetype !== 'application/vnd.android.package-archive') {
     return next(new Error('LIMIT_FILE_TYPES'));
   }
 
-  if (!fs.existsSync(pathTmp)) fs.mkdirSync(pathTmp);
   return next(null, true);
 };
 
@@ -41,21 +40,20 @@ const upload = multer({
 
 routes.post('/upload', upload.single('file'), async (req, res, next) => {
   try {
-    if (!fs.existsSync(pathUploads)) fs.mkdirSync(pathUploads);
-    await rename(fullPathAppTmp, fullPathApp);
+    await fsPromises.rename(fullPathAppTmp, fullPathApp);
     const reader = await ApkReader.open(fullPathApp);
     const manifest = await reader.readManifest();
     const { versionName } = manifest;
-    await models.App.update({ version: versionName }, { where: { id: { [Op.eq]: 1 } } });
+    await models.App.update({ version: versionName }, { where: { id: 1 } });
   } catch (err) {
-    next(new Error(err));
+    return next(new Error(err));
   }
-  res.status(204).send();
+  return res.status(204).send();
 });
 
 routes.get('/check/:version', async (req, res, next) => {
   const { version } = req.params;
-  const app = await models.App.findOne({ where: { id: { [Op.eq]: 1 } }, attributes: ['version'] });
+  const app = await models.App.findOne({ where: { id: 1 }, attributes: ['version'] });
   const status = compareVersion(app.version, version) > 0;
   res.send({ status });
 });
